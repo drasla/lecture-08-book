bd7b78a7f64be4c0e08fab15c7d0960ea4ea605c
Tae.Garam <drasla@naver.com>
2025-12-05 12:47:55 +0900

    Search 코드 작성 완료

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,22 @@
 import { useSearchParams } from "react-router";
+import { useEffect, useState } from "react";
+
+type BookItem = {
+    id: string;
+    volumeInfo: {
+        title: string;
+        authors?: string[];
+        description?: string;
+        imageLinks?: {
+            thumbnail?: string;
+            smallThumbnail?: string;
+        };
+    };
+};
+
+type ApiResponse = {
+    items: BookItem[];
+};
 
 function Search() {
     // 해야되는 일 :
@@ -14,8 +32,21 @@ function Search() {
     // 그에 대한 처리를 논리합을 통해 처리
     const keyword = params.get("keyword") || "";
 
+    const [list, setList] = useState<BookItem[]>([]);
 
-    return <>Search</>
+    useEffect(() => {
+        // queryString이 빈 값이 들어올 때를 고려
+        // queryString이 없으면, 뒤를 실행하지 않고 if에서 종료 처리
+        if (!keyword) return;
+
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=20`)
+            .then(response => response.json())
+            .then((data: ApiResponse) => {
+                setList(data.items)
+            });
+    }, []);
+
+    return <>{keyword}</>;
 }
 
 export default Search;
